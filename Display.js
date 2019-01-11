22e3f644f41171f858ca18035ab00776265df85a
nicholy <43461092+nicholy@users.noreply.github.com>
2019-01-10 18:29:29 -0800

    Add files via upload

---
+++ Display.js
@@ -0,0 +1,19 @@
+// JavaScript source code
+
+
+var display = function display() {
+
+    this.rows = 32;
+    this.columns = 64;
+    this.resolution = this.rows * thiscolumns;
+    this.bitMap = new Array(this.resolution);
+
+    @method clear;
+
+    this.clear = function {
+
+        this.bitMap = new Array(this.resolution);
+    }
+
+
+}
