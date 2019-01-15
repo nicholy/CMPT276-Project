274eabb4a8e5c113a50886509f7cb8206fcabdea
Nichol Yip <nicholyip@Nichols-MBP.telus>
2019-01-14 21:08:07 -0800

    added opcode 0x7000 and 0x8000 (0-6)

--- chip8.js
+++ chip8.js
@@ -6,14 +6,14 @@
 let p = document.querySelector("p");
 
 function chip8(){
-  this.pc = 0;
+  this.pc = 0x200;
   this.stack = new Array(16);
   this.memory = new Uint8Array(4096);
   this.v = new Uint8Array(16);
   this.startAddr = 0x200;
   this.displayWidth = 64;
   this.displayHeight = 32;
-  this.resolution = this.displayWidth * this.displayHeight
+  this.resolution = this.displayWidth * this.displayHeight;
   this.display = new Array(resolution);
   this.i = null;
   this.sp = null;
@@ -68,7 +68,7 @@ console.log(memory);
 var a = new chip8();
 p.textContent = a.memory;
 
-emulateCycle: function () {
+emulateCycle: function(){
 
   var opcode = this.memory[this.pc] << 8 | this.memory[this.pc+1];
   var x = (opcode & 0x0F00) >> 8;
@@ -77,6 +77,55 @@ emulateCycle: function () {
   this.pc += 2;
 
   switch(opcode & 0xF000) {
+
+    case 0x7000:
+      this.v[x] = this.v[x] + (opcode & 0x00FF);
+      break;
+
+    case 0x8000:
+
+      switch (opcode & 0x000F) {
+
+        case 0x0000:
+          this.v[x] = this.v[y];
+          break;
+        case 0x0001:
+          this.v[x] = this.v[x] | this.v[y];
+          break;
+        case 0x0002:
+          this.v[x] = this.v[x] & this.v[y];
+          break;
+        case 0x0003:
+          this.v[x] = this.v[x] ^ this.v[y];
+          break;
+        case 0x0004:
+          var sum = this.v[x] + this.v[y];
+
+          if (sum > 0xFF){
+            this.v[0xF] = 1;
+          }
+          else {
+            this.v[0xF] = 0;
+          }
+
+          this.v[x] = sum;
+          break;
+        case 0x0005:
+
+          if(this.v[x] > this.v[y]){
+            this.v[0xF] = 1;
+          }
+          else {
+            this.v[0xF] = 0;
+          }
+
+          this.v[x] = this.v[x] = this.v[y];
+          break;
+
+        case 0x0006:
+          this.v[0xF] = this.v[x] & 0x01;
+          this.v[x] = v[x] >> 1;
+      }
 
   }
 }
