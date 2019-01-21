8f0e0844e583a63d292acbd36cf703e66d66b4c9
root <root@DESKTOP-3K8PVHI.localdomain>
2019-01-21 11:13:36 -0800

    Added opcodes 0xF007 to 0xFX65

--- chip8.js
+++ chip8.js
@@ -17,11 +17,13 @@ function chip8(){
   this.display = new Array(resolution);
   this.i = null;
   this.sp = null;
+  this.delayTimer = null;
+  this.soundTimer = null;
 }
 
 function program(){
-  	for (i = 0x200; i < 0x06A0; i++) {
-    	this.memory[i] = 1;
+    for (i = 0x200; i < 0x06A0; i++) {
+      this.memory[i] = 1;
     }
 }
 
@@ -50,8 +52,8 @@ function display(){
 }
 
 function opcode1(){
-	for (var i = 0; i < this.display.length; i++) {
-    	this.display[i] = 0;
+  for (var i = 0; i < this.display.length; i++) {
+      this.display[i] = 0;
     }
 }
 
@@ -64,7 +66,7 @@ variable();
 display();
 opcode1();
 console.log(memory);
-  
+
 
 function emulationcycle(){
 
@@ -75,89 +77,95 @@ function emulationcycle(){
   this.pc += 2;
 
   switch(opcode & 0xF000) {
-    case 0x0000:
 
-	switch (opcode) {
-		case 0x00E0:
-			for (var i = 0; i < this.display.length; i++) {
-				this.display[i] = 0;
-			}
-			break;
-		case 0x00EE:
-                	this.pc = this.stack[--this.sp];
-                	break;      
-        }
-	break;
-		  
+    case 0x0000:
+      switch (opcode) {
+
+        case 0x00E0:
+          for (var i = 0; i < this.display.length; i++) {
+            this.display[i] = 0;
+          }
+          break;
+
+        case 0x00EE:
+          this.pc = this.stack[--this.sp];
+          break;
+      }
+      break;
+
     case 0x1000:
-	this.pc = opcode & 0xFFF;
-        break;  
+      this.pc = opcode & 0xFFF;
+      break;
+
     case 0x2000:
-            this.stack[this.sp] = this.pc;
-            this.sp++;
-            this.pc = opcode & 0x0FFF;
-            break;
-     case 0x3000:
-            if (this.v[x] === (opcode & 0xFF)) {
-            	this.pc += 2;
-            }
-            break;
-     case 0x4000:
-			if (this.v[x] != (opcode & 0x00FF)) {
-				this.pc += 2;
-			}
-			break;
-     case 0x5000:
-			if (this.v[x] === this.v[y]) {
-				this.pc += 2;
-			}
-			break;
-     case 0x6000:
-			this.v[x] = opcode & 0xFF;
-			break;
-  
-	
+      this.stack[this.sp] = this.pc;
+      this.sp++;
+      this.pc = opcode & 0x0FFF;
+      break;
+
+    case 0x3000:
+      if (this.v[x] === (opcode & 0xFF)) {
+        this.pc += 2;
+      }
+      break;
+
+    case 0x4000:
+      if (this.v[x] != (opcode & 0x00FF)) {
+        this.pc += 2;
+      }
+      break;
+
+    case 0x5000:
+      if (this.v[x] === this.v[y]) {
+        this.pc += 2;
+      }
+      break;
+
+    case 0x6000:
+      this.v[x] = opcode & 0xFF;
+      break;
+
     case 0x7000:
       this.v[x] = this.v[x] + (opcode & 0x00FF);
       break;
 
     case 0x8000:
-
       switch (opcode & 0x000F) {
 
         case 0x0000:
           this.v[x] = this.v[y];
           break;
+
         case 0x0001:
           this.v[x] = this.v[x] | this.v[y];
           break;
+
         case 0x0002:
           this.v[x] = this.v[x] & this.v[y];
           break;
+
         case 0x0003:
           this.v[x] = this.v[x] ^ this.v[y];
           break;
+
         case 0x0004:
           var sum = this.v[x] + this.v[y];
-
           if (sum > 0xFF){
             this.v[0xF] = 1;
           }
           else {
             this.v[0xF] = 0;
           }
-
           this.v[x] = sum;
           break;
-        case 0x0005:
 
+        case 0x0005:
           if(this.v[x] > this.v[y]){
             this.v[0xF] = 1;
           }
           else {
             this.v[0xF] = 0;
           }
-
           this.v[x] = this.v[x] = this.v[y];
           break;
 
@@ -165,10 +173,45 @@ function emulationcycle(){
           this.v[0xF] = this.v[x] & 0x01;
           this.v[x] = v[x] >> 1;
       }
+    case 0xF000:
+      switch(opcode & 0x00FF){
+        case 0x0007:
+          this.v[x] = this.delayTimer;
+          break;
+        //CASE 0X000A NOT COMPLETED, NEED HEX KEYBOARD
+        case 0x000A:
+          break;
 
+        case 0x0015:
+          this.delayTimer = this.v[x];
+          break;
+        case 0x0018:
+          this.soundTimer = this.v[x];
+          break;
+        case 0x001E:
+          this.i += this.v[x];
+          break;
+        case 0x0029:
+          this.i = this.v[x] * 5;
+          break;
+        case 0x0033:
+          this.memory[this.i] = (this.v[x] - (this.v[x] % 100)) / 100;
+          this.memory[this.i + 1] = (this.v[x] % 100 - (this.v[x] % 10)) / 10;
+          this.memory[this.i + 2] = this.v[x] % 10;
+          break;
+        case 0x0055:
+          for (var i = 0; i < x; i++){
+            this.memory[this.i + i] = this.v[i];
+          }
+          break;
+        case 0x0065:
+          for (var i = 0; i < x; i++){
+            this.v[i] = this.memory[this.i + i];
+          }
+          break;
+      }
   }
 }
-
 
 
 
