8339798fc6bfe4aac810bae4081c09007eb8e849
Nichol Yip <nicholyip@d142-058-215-094.wireless.sfu.ca>
2019-01-28 10:58:40 -0800

    added macs portion

--- chip8.js
+++ chip8.js
@@ -1,18 +1,120 @@
 var CHIP8 = (function() {
 
+  // Program counter
   this.pc = 0x200;
+
+  // Stack and stack pointer
   this.stack = new Array(16);
+  this.sp = 0;
+
+  // Memory space
   this.memory = new Uint8Array(4096);
+
+  // Registers
   this.v = new Uint8Array(16);
+  this.i = 0;
+
+  // IDK what this is
   this.startAddr = 0x200;
+
+  // Graphics 64 * 32
   this.displayWidth = 64;
   this.displayHeight = 32;
   this.resolution = this.displayWidth * this.displayHeight;
   this.display = new Array(resolution);
-  this.i = null;
-  this.sp = null;
-  this.delayTimer = null;
-  this.soundTimer = null;
+
+  // Timers
+  this.delayTimer = 0;
+  this.soundTimer = 0;
+
+  this.reset();
+
+  chip8.prototype = {
+    loadProgram: function(program) {
+      // Load program into memory
+      for (i = 0; i < program.length; i++) {
+        this.memory[i + 0x200] = program[i];
+      }
+
+      //it create an string and convert memroy into hex and put into string [.toString(16)] with newline break for each 24 character
+      var output = "";
+      for (var i = 0; i < ch.memory.length; i++) {
+        output += ch.memory[i].toString(16) + " ";
+        if (!((i + 1) % 24)) {
+          output += "\n";
+        }
+      }
+      p.textContent = output;
+    },
+
+    visualizer: function() {
+      for (i = 0; i < this.memory.length; i++) {
+        if (i < 0x200) {
+          this.memory[i] = 1;
+        } else if (i < 0x06A0) {
+          this.memory[i] = 2;
+        } else if (i < 0x06D0) {
+        this.memory[i] = 3;
+        } else if (i < 0x06F0) {
+          this.memory[i] = 4;
+        } else if (i < 0x0700) {
+          this.memory[i] = 5;
+        } else {
+          this.memory[i] = 6;
+        }
+      }
+    },
+
+    reset: function() {
+      var i;
+      // Reset memory.
+      for (i = 0; i < this.memory.length; i++) {
+        this.memory[i] = 0;
+      }
+
+      var hexChars = [
+        0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+        0x20, 0x60, 0x20, 0x20, 0x70, // 1
+        0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+        0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+        0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+        0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+        0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+        0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+        0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+        0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+        0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+        0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+        0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+        0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+        0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+        0xF0, 0x80, 0xF0, 0x80, 0x80 // F
+      ];
+
+      for (i = 0; i < hexChars.length; i++) {
+        this.memory[i] = hexChars[i];
+      }
+
+
+      // Reset registers.
+      for (i = 0; i < this.v.length; i++) {
+        this.v[i] = 0;
+      }
+
+      // Reset display.
+      for (i = 0; i < this.display.length; i++) {
+        this.display[i] = 0;
+      }
+
+      // Reset stack pointer, I
+      this.sp = 0;
+      this.i = 0;
+
+      // The program counter starts at 0x200, as
+      // that is the start location of the program.
+      this.pc = 0x200;
+    }
+  };
 
 
   function loadProgram(program){
@@ -48,32 +150,6 @@ var CHIP8 = (function() {
   function opcode1(){
     for (var i = 0; i < this.display.length; i++) {
         this.display[i] = 0;
-    }
-  }
-
-  var init = function() {
-
-    var sprites = [
-      0xF0, 0x90, 0x90, 0x90, 0xF0, //0
-      0x20, 0x60, 0x20, 0x20, 0x70, //1
-      0xF0, 0x10, 0xF0, 0x80, 0xF0, //2
-      0xF0, 0x10, 0xF0, 0x10, 0xF0, //3
-      0x90, 0x90, 0xF0, 0x10, 0x10, //4
-      0xF0, 0x80, 0xF0, 0x10, 0xF0, //5
-      0xF0, 0x80, 0xF0, 0x90, 0xF0, //6
-      0xF0, 0x10, 0x20, 0x40, 0x40, //7
-      0xF0, 0x90, 0xF0, 0x90, 0xF0, //8
-      0xF0, 0x90, 0xF0, 0x10, 0xF0, //9
-      0xF0, 0x90, 0xF0, 0x90, 0x90, //A
-      0xE0, 0x90, 0xE0, 0x90, 0xE0, //B
-      0xF0, 0x80, 0x80, 0x80, 0xF0, //C
-      0xE0, 0x90, 0x90, 0x90, 0xE0, //D
-      0xF0, 0x80, 0xF0, 0x80, 0xF0, //E
-      0xF0, 0x80, 0xF0, 0x80, 0x80, //F
-    ];
-
-    for ( var i = 0; i < sprites.length; i++ ) {
-      memory[i] = sprites[i];
     }
   }
 
